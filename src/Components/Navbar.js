b3f23870bda9ea83213fdd5973691cdd61b52344
sami <84959599+Samikhan5823@users.noreply.github.com>
2022-05-18 16:02:13 +0500

    updated

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,12 +4,10 @@ import {
   FaTimes,
   FaGithub,
   FaLinkedin,
-  FaFacebook,
-  FaLinkedinIn,
+ 
 } from 'react-icons/fa';
 import { HiOutlineMail } from 'react-icons/hi';
 import { BsFillPersonLinesFill } from 'react-icons/bs';
-import Logo from '../assets/logo.png';
 import { Link } from 'react-scroll';
 
 const Navbar = () => {
